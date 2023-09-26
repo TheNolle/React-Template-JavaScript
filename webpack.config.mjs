90ac690b3e2c974bfb8403c6948758fabe3ef6ed
TheNolle <github@thenolle.com>
2023-09-26 15:34:57 +0200

    Fixed few issues with Build version (now working + guide in console)

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import WebpackShellPluginNext from 'webpack-shell-plugin-next'
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -38,6 +39,25 @@ export const plugins = [
     new HtmlWebpackPlugin({
         template: './public/index.html',
         favicon: './public/favicon.ico',
+    }),
+    new WebpackShellPluginNext({
+        onBeforeBuild: {
+            scripts: [
+                'rm -rf dist',
+                'clear',
+            ],
+            blocking: true,
+            parallel: false,
+        },
+        onAfterDone: {
+            scripts: [
+                `node -e "const fs = require('fs'); const content = 'RewriteEngine On\\n\\n# If an existing asset or directory is requested go to it as it is\\nRewriteCond %%{DOCUMENT_ROOT}%%{REQUEST_URI} -f [OR]\\nRewriteCond %%{DOCUMENT_ROOT}%%{REQUEST_URI} -d\\nRewriteRule ^ - [L]\\n\\n# If the requested resource doesn\\'t exist, use index.html\\nRewriteRule ^ /index.html\\n\\n\\n# © 2023 Nolly | Website: thenolle.com | Github: @TheNolle | Patreon: @_nolly'; fs.writeFileSync('dist/.htaccess', content);"`,
+                `node -e "(async () => { const chalk = await import('chalk'); console.log(chalk.default.green('\\n\\n\\n\\n\\nBuild Successful!')); console.log(chalk.default.cyan('\\nTo serve the built project on Apache:')); console.log(chalk.default.yellow('1. Copy the dist folder contents to your Apache directory.')); console.log(chalk.default.yellow('2. Ensure mod_rewrite is enabled: sudo a2enmod rewrite')); console.log(chalk.default.yellow('3. Restart Apache: sudo systemctl restart apache2')); console.log(chalk.default.yellow('4. Ensure .htaccess overrides are allowed with: AllowOverride All')); console.log(chalk.default.red('\\nFor detailed deployment steps, refer to the official Apache documentation.')); })();"`,
+                `node -e "(async () => { const chalk = await import('chalk'); console.log(chalk.default.magenta('\\n\\n\\n\\n© 2023 Nolly | Website: thenolle.com | Github: @TheNolle | Patreon: @_nolly\\n')); })();"`,
+            ],
+            blocking: true,
+            parallel: false,
+        },
     }),
 ]
 
